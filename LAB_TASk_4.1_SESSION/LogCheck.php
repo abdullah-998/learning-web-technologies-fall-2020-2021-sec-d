5115d64571deab457dbdef908c31d3742a8d8ea7
unknown <abdullah.jayedoo@gmail.com>
2020-11-18 19:15:02 +0600

    LogCheck done

--- LAB_TASk_4.1_SESSION/LogCheck.php
+++ LAB_TASk_4.1_SESSION/LogCheck.php
@@ -2,13 +2,16 @@
     session_start();
     if(isset($_REQUEST['submit']))
     {
-        $name=$_REQUEST['name'];
-        $pass=$_REQUEST['pass'];
+        $name=$_COOKIE['name'];
+        $pass=$_COOKIE['pass'];
+
+        $sname=$_SESSION['name'];
+        $spass=$_SESSION['pass'];
         if($name=="" || $pass=="")
         {
             header('location: Log.php?msg=null');
         }
-        if($name==$pass)
+        if($name==$sname && $pass==$spass)
         {
             $_SESSION['name']=$name;
             header("location: Dashboard.php?msg=log");
