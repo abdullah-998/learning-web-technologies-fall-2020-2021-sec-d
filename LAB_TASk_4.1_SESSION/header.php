f7dd49af0fabf1642f7410b4ab7ade24a8b62af2
unknown <abdullah.jayedoo@gmail.com>
2020-11-25 20:41:36 +0600

    Header done

--- LAB_TASk_4.1_SESSION/header.php
+++ LAB_TASk_4.1_SESSION/header.php
@@ -1,5 +1,6 @@
   <?php
     echo '<img src="logo.png" height="60px" width="100px">
     <a href="Home.php">Home|</a>
-    <a href="Log.php">Login</a>';
+    <a href="Log.php">Login</a>|
+    <a href="Log.php">Registration</a>';
 ?>
