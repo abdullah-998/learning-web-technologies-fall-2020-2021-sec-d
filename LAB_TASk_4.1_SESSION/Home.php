6b71b6c130a5fddb9cb0bfba85345e7db01160e5
unknown <abdullah.jayedoo@gmail.com>
2020-11-25 21:18:45 +0600

    home done

---
+++ LAB_TASk_4.1_SESSION/Home.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table border="1" align="center">
+        <tr>
+            <td height="60px" width="700px">
+                <?php include 'header.php' ?>
+            </td>
+        </tr>
+        <tr>
+            <td height="200px" width="700px" align="left">
+                <h1>Welcome to xCompany</h1>
+            </td>
+        </tr>
+        <tr>
+            <td height="60px" width="700px" align="center">Copyrignt &#169 2017</td>
+        </tr>
+    </table>
+</body>
+</html>
