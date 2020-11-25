f299f292649e3f519c87357832cd6e74ee1cc195
unknown <abdullah.jayedoo@gmail.com>
2020-11-25 20:38:48 +0600

    php 2 done

---
+++ LAB_TASk_4.1_SESSION/sidebar.php
@@ -0,0 +1,6 @@
+<li><a href="Dashboard.php">Dashboard</a><br></li>
+<li><a href="Profile.php">View Profile</a><br></li>
+<li><a href="EProfile.php">Edit Profile</a><br></li>
+<li><a href="CPic.php">Change Profile Picture</a><br></li>
+<li><a href="CPass.php">Change Password</a><br></li>          
+<li><a href="Logout.php">Log out</a><br></li>    
