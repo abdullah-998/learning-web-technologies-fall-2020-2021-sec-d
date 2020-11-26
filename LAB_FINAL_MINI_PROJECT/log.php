81d3ded14984f2dfa4bee8a0a1ed82c30a722adf
unknown <abdullah.jayedoo@gmail.com>
2020-11-26 14:34:15 +0600

    log done

---
+++ LAB_FINAL_MINI_PROJECT/log.php
@@ -0,0 +1,38 @@
+<?php
+    session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Log in</title>
+</head>
+<body>
+    <form action="logCheck.php" method="POST">
+        <fieldset>
+            <legend>LOGIN</legend>
+            User ID<br>
+            <input type="text" name="id"><br>
+            Password<br>
+            <input type="password" name="pass"><br><hr>
+            <input type="submit" name="submit">
+            <a href="Reg.php">Sign Up</a><br>
+            <?php
+                if(isset($_REQUEST['msg']))
+                {
+                    if($_REQUEST['msg']=='null')
+                    {
+                        echo 'null submission';
+                    }
+                    if($_REQUEST['msg']=='inv')
+                    {
+                        echo 'invalid name or password';
+                    }
+                }
+
+            ?>
+        </fieldset>
+    </form>
+</body>
+</html>
