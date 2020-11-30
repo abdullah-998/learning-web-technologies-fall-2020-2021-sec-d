340246e6d4b1e02b33e7220c99b2bde5d742d5dd
unknown <abdullah.jayedoo@gmail.com>
2020-11-30 19:12:28 +0600

    header done

---
+++ FINAL_LAB_TASK_1.1_DB/view/header.php
@@ -0,0 +1,6 @@
+  <?php
+    echo '<img src="../asset/logo.png" height="60px" width="100px">
+    <a href="Home.php">Home|</a>
+    <a href="Log.php">Login</a>|
+    <a href="Reg.php">Registration</a>';
+?>
