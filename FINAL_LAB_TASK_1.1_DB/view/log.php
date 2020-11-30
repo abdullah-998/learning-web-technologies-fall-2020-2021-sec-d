b7ec8d4966fdde8f1fa0b5a7381c3a2736897228
unknown <abdullah.jayedoo@gmail.com>
2020-11-30 19:18:34 +0600

    log done

---
+++ FINAL_LAB_TASK_1.1_DB/view/log.php
@@ -0,0 +1,53 @@
+<?php
+    session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <table border="1" align="center">
+        <tr>
+            <td height="60px" width="700px">
+                <?php include 'header.php' ?>
+            </td>
+        </tr>
+        <tr>
+            <?php
+            ?>
+            <td height="200px" width="700px" align="left">
+                <form action="../asset/LogCheck.php" method="POST">
+                    <fieldset>
+                        <legend>Log in</legend>
+                        Username &nbsp;:<input type="text" name="name"><br><br>
+                        Password &nbsp;&nbsp;:<input type="password" name="pass"><br>
+                        <hr>
+                        <input type="checkbox">remember me?<br>
+                        <input type="submit" name="submit">
+                        <a href="Cpass.php">Change password?</a>
+                    </fieldset>
+                    <?php
+                    if(isset($_REQUEST['msg']))
+                    {
+                        if($_REQUEST['msg']=='null')
+                        {
+                            echo "null submission..";
+                        }
+                        if($_REQUEST['msg']=='match')
+                        {
+                            echo "pasword or username is not valid..";
+                        }
+                    }
+                ?>
+                </form>
+            </td>
+        </tr>
+        <tr>
+            <td height="60px" width="700px" align="center">Copyrignt &#169 2017</td>
+        </tr>
+    </table>
+</body>
+</html>
