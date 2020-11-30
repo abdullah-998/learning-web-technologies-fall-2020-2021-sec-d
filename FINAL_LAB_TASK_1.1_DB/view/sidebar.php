f740006b0c9e8674aa013b7108df9318dcdfcf54
unknown <abdullah.jayedoo@gmail.com>
2020-11-30 19:39:51 +0600

    sidebar done

---
+++ FINAL_LAB_TASK_1.1_DB/view/sidebar.php
@@ -0,0 +1,7 @@
+<li><a href="Dashboard.php">Dashboard</a><br></li>
+<li><a href="Profile.php">View Profile</a><br></li>
+<li><a href="EProfile.php">Edit Profile</a><br></li>
+<li><a href="CPic.php">Change Profile Picture</a><br></li>
+<li><a href="CPass.php">Change Password</a><br></li>          
+<li><a href="../php/Logout.php">Log out</a><br></li>    
+<li><a href="alluser.php">All User</a></li>
