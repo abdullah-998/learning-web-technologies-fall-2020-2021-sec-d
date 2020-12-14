f205dd47a78bb1fb641c4d024959b38f0f5fb65e
unknown <abdullah.jayedoo@gmail.com>
2020-12-14 10:17:52 +0600

    script done and updated

--- Final_Lab Task_Ajax_4.1/controller/script.js
+++ Final_Lab Task_Ajax_4.1/controller/script.js
@@ -65,3 +65,20 @@ function alluser()
         }
     })
 }
+
+function chngpass()
+{
+    let req= new XMLHttpRequest();
+    req.open('GET','../controller/chngpass.php',true);
+    req.send();
+
+    let inf=document.querySelector('#info')
+
+    req.addEventListener('readystatechange',()=>
+    {
+        if(req.readyState==4 && req.status==200)
+        {
+            inf.innerHTML=req.responseText;
+        }
+    })
+}
